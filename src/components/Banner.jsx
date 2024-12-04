be05c2a7b4120f376478d42e4f72193ecc4c7f59
sumu9897 <sumon.bncd@gmail.com>
2024-12-04 20:42:43 +0600

    All component add

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <div>
+            Banner Part
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/components/CampaignCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Campaigncard() {
+    return (
+        <div>
+            Card Campaign 
+        </div>
+    )
+}
+
+export default Campaigncard
